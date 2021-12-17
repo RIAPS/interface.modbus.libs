7e07709fded5759b0b3eaaf10810e2f6cbd560c6
GWMAC150 <gerald.mcafee@vanderbilt.edu>
2021-12-16 21:16:26 -0600

    Added handling for empty device name

--- ModbusDevice.py
+++ ModbusDevice.py
@@ -100,7 +100,12 @@ class ModbusDevice(Component):
         msg_bytes = self.device_port.recv()  # required to remove message from queue
         msg = device_capnp.DeviceQry.from_bytes(msg_bytes)
 
-        dthd =  self.devices[ msg.device ]
+        dvcname = msg.device
+        # if the device name is empty then use the first device in the list of keys
+        if dvcname == "" :
+            dvcname = self.modbus_device_keys[0]
+
+        dthd =  self.devices[ dvcname ]
         plug_identity = self.modbus_cmd_port.get_plug_identity( dthd.get_plug() )
         self.modbus_cmd_port.set_identity( plug_identity )
         self.modbus_cmd_port.send_pyobj( msg )
@@ -113,186 +118,186 @@ class ModbusDevice(Component):
     # riaps:keep_poller:end
 
     # riaps:keep_modbus_poller:begin
-    def on_modbus_poller(self):
-        """Poll all variable names specified in yaml file"""
+    # def on_modbus_poller(self):
+    #     """Poll all variable names specified in yaml file"""
 
-        now = self.modbus_poller.recv_pyobj()
-        if self.dvc["debugMode"]:
-            self.logger.info(f"on_modbus_poller now: {now}")
-        comm_error = False
-        time_over_run = False
+    #     now = self.modbus_poller.recv_pyobj()
+    #     if self.dvc["debugMode"]:
+    #         self.logger.info(f"on_modbus_poller now: {now}")
+    #     comm_error = False
+    #     time_over_run = False
 
-        if self.dvc["poll"]:
-            for var in self.dvc["poll"]:
-                modbus_evt = device_capnp.DeviceEvent.new_message()
-                modbus_evt.device = self.device_name
-                # Prepare status message to send on pub port.
+    #     if self.dvc["poll"]:
+    #         for var in self.dvc["poll"]:
+    #             modbus_evt = device_capnp.DeviceEvent.new_message()
+    #             modbus_evt.device = self.device_name
+    #             # Prepare status message to send on pub port.
 
-                if not self.poll_exit and not comm_error and not time_over_run:
-                    t1 = datetime.datetime.now()
-                    values = []
-                    names = []
-                    units = []
-                    post_event = False
+    #             if not self.poll_exit and not comm_error and not time_over_run:
+    #                 t1 = datetime.datetime.now()
+    #                 values = []
+    #                 names = []
+    #                 units = []
+    #                 post_event = False
 
-                    # setup limits if they exist
-                    if self.dvc["poll"][var]:
-                        max_threshold = self.dvc["poll"][var][0]
-                        min_threshold = self.dvc["poll"][var][1]
-                    else:
-                        max_threshold = None
-                        min_threshold = None
+    #                 # setup limits if they exist
+    #                 if self.dvc["poll"][var]:
+    #                     max_threshold = self.dvc["poll"][var][0]
+    #                     min_threshold = self.dvc["poll"][var][1]
+    #                 else:
+    #                     max_threshold = None
+    #                     min_threshold = None
 
-                    # Construct message from yaml file
-                    # The message fields are defined in the capnp file
-                    func = self.dvc[var]
-                    function_code = func['function']
-                    starting_address = func['start']
-                    length = func['length']
-                    if 'data_format' in list(func.keys()):
-                        data_fmt = func['data_format']
-                    else:
-                        data_fmt = ''
+    #                 # Construct message from yaml file
+    #                 # The message fields are defined in the capnp file
+    #                 func = self.dvc[var]
+    #                 function_code = func['function']
+    #                 starting_address = func['start']
+    #                 length = func['length']
+    #                 if 'data_format' in list(func.keys()):
+    #                     data_fmt = func['data_format']
+    #                 else:
+    #                     data_fmt = ''
 
-                    # See if this is a bit read
-                    if 'bit_position' in list(func.keys()):
-                        bit_pos = func['bit_position']
-                    else:
-                        bit_pos = -1
+    #                 # See if this is a bit read
+    #                 if 'bit_position' in list(func.keys()):
+    #                     bit_pos = func['bit_position']
+    #                 else:
+    #                     bit_pos = -1
 
-                    modbus_evt.command = var
+    #                 modbus_evt.command = var
 
-                    # Send command to Modbus slave
-                    try:
-                        modbus_response_uint = self.master.execute(self.slave,
-                                                                   getattr(cst, function_code),
-                                                                   starting_address,
-                                                                   quantity_of_x=length,
-                                                                   data_format=data_fmt)
-                        # fill out more status fields
-                        modbus_evt.event = 'POLLED'
-                        modbus_evt.error = 0
-                        # self.logger.info(f"{helper.Cyan}\ncall: {var}; response: {str(modbus_response_uint)}\n{helper.RESET}")
+    #                 # Send command to Modbus slave
+    #                 try:
+    #                     modbus_response_uint = self.master.execute(self.slave,
+    #                                                                getattr(cst, function_code),
+    #                                                                starting_address,
+    #                                                                quantity_of_x=length,
+    #                                                                data_format=data_fmt)
+    #                     # fill out more status fields
+    #                     modbus_evt.event = 'POLLED'
+    #                     modbus_evt.error = 0
+    #                     # self.logger.info(f"{helper.Cyan}\ncall: {var}; response: {str(modbus_response_uint)}\n{helper.RESET}")
 
-                        # convert Modbus response from uint to int
-                        if data_fmt == '':
-                            # modbus_response = (np.array(modbus_response_uint, dtype="uint16").view(np.int16))
-                            modbus_response = list(
-                                map(lambda ui:
-                                    struct.unpack("h",
-                                                  struct.pack("H", ui))[0],
-                                    modbus_response_uint))
-                        else:
-                            modbus_response = modbus_response_uint
-                            # self.logger.info( "modbus_response: {0}".format( modbus_response ) )
+    #                     # convert Modbus response from uint to int
+    #                     if data_fmt == '':
+    #                         # modbus_response = (np.array(modbus_response_uint, dtype="uint16").view(np.int16))
+    #                         modbus_response = list(
+    #                             map(lambda ui:
+    #                                 struct.unpack("h",
+    #                                               struct.pack("H", ui))[0],
+    #                                 modbus_response_uint))
+    #                     else:
+    #                         modbus_response = modbus_response_uint
+    #                         # self.logger.info( "modbus_response: {0}".format( modbus_response ) )
 
-                    except Exception as ex:
-                        if self.dvc["debugMode"]:
-                            self.logger.info('Modbus Exception: {0}'.format(ex))
-                        modbus_evt.event = 'ERROR'
-                        modbus_evt.error = -1
-                        names = ['Exception: {0}'.format(ex), ]
-                        units = ['ERROR', ]
-                        values = [-1.0, ]
-                        modbus_response = [modbus_evt.error, ]
-                        min_threshold = None
-                        max_threshold = None
-                        comm_error = True
-                        if self.dvc["debugMode"]:
-                            self.logger.info(f'Modbus response error {str(modbus_response)}')
+    #                 except Exception as ex:
+    #                     if self.dvc["debugMode"]:
+    #                         self.logger.info('Modbus Exception: {0}'.format(ex))
+    #                     modbus_evt.event = 'ERROR'
+    #                     modbus_evt.error = -1
+    #                     names = ['Exception: {0}'.format(ex), ]
+    #                     units = ['ERROR', ]
+    #                     values = [-1.0, ]
+    #                     modbus_response = [modbus_evt.error, ]
+    #                     min_threshold = None
+    #                     max_threshold = None
+    #                     comm_error = True
+    #                     if self.dvc["debugMode"]:
+    #                         self.logger.info(f'Modbus response error {str(modbus_response)}')
 
-                    # if there is an error the length of the list will always be 1
-                    # Must distinguish between a single value register or single value float, word, dword
-                    # if data_fmt = '' we are dealing with a single register
-                    # otherwise this is a data type greater than a 16bit value
-                    if len(modbus_response) == 1 and data_fmt == '':
-                        if modbus_evt.event == 'POLLED':
-                            # only do a bit mask operation for single register polling
-                            if bit_pos != -1:
-                                # only return the state of the bit we are interested in
-                                if modbus_response[0] & (1 << bit_pos) != 0:
-                                    modbus_response[0] = True  # if the bit is set return True
-                                else:
-                                    modbus_response[0] = False
-                                scaler = 1.0
-                                units = ['', ]
-                            else:
-                                scaler = float(func['Units'][0])
-                                units = [(func['Units'][1]), ]
+    #                 # if there is an error the length of the list will always be 1
+    #                 # Must distinguish between a single value register or single value float, word, dword
+    #                 # if data_fmt = '' we are dealing with a single register
+    #                 # otherwise this is a data type greater than a 16bit value
+    #                 if len(modbus_response) == 1 and data_fmt == '':
+    #                     if modbus_evt.event == 'POLLED':
+    #                         # only do a bit mask operation for single register polling
+    #                         if bit_pos != -1:
+    #                             # only return the state of the bit we are interested in
+    #                             if modbus_response[0] & (1 << bit_pos) != 0:
+    #                                 modbus_response[0] = True  # if the bit is set return True
+    #                             else:
+    #                                 modbus_response[0] = False
+    #                             scaler = 1.0
+    #                             units = ['', ]
+    #                         else:
+    #                             scaler = float(func['Units'][0])
+    #                             units = [(func['Units'][1]), ]
 
-                            formatted_value = float(modbus_response[0])
-                            formatted_value = formatted_value * scaler
-                            names = [var, ]
-                            values = [formatted_value, ]
-                        else:
-                            pass
-                    else:
-                        # reading many different values
-                        if data_fmt == '':
-                            """ put the values into the riaps message """
-                            d = self.format_multi_register_read(
-                                starting_address,
-                                length,
-                                self.dvc,
-                                modbus_response)
+    #                         formatted_value = float(modbus_response[0])
+    #                         formatted_value = formatted_value * scaler
+    #                         names = [var, ]
+    #                         values = [formatted_value, ]
+    #                     else:
+    #                         pass
+    #                 else:
+    #                     # reading many different values
+    #                     if data_fmt == '':
+    #                         """ put the values into the riaps message """
+    #                         d = self.format_multi_register_read(
+    #                             starting_address,
+    #                             length,
+    #                             self.dvc,
+    #                             modbus_response)
 
-                            names = list(d.keys())
-                            for n in names:
-                                values.append(d[n]['Value'])
-                                units.append(d[n]['Units'])
-                            min_threshold = None
-                            max_threshold = None
-                        else:  # reading a single value that was multiple registers
-                            names = [var, ]
-                            values = [modbus_response[0], ]
-                            units = [(func['Units'][1]), ]
+    #                         names = list(d.keys())
+    #                         for n in names:
+    #                             values.append(d[n]['Value'])
+    #                             units.append(d[n]['Units'])
+    #                         min_threshold = None
+    #                         max_threshold = None
+    #                     else:  # reading a single value that was multiple registers
+    #                         names = [var, ]
+    #                         values = [modbus_response[0], ]
+    #                         units = [(func['Units'][1]), ]
 
-                    # if no limits then always post the modbus response
-                    if min_threshold is None and max_threshold is None:
-                        post_event = True
-                    else:  # if there are limits then evaluate them
-                        if max_threshold is not None:
-                            if values[0] > max_threshold:  # if value is greater than limit then post event
-                                post_event = True
-                        if min_threshold is not None:
-                            if values[0] < min_threshold:  # if value is less than limit post event
-                                post_event = True
+    #                 # if no limits then always post the modbus response
+    #                 if min_threshold is None and max_threshold is None:
+    #                     post_event = True
+    #                 else:  # if there are limits then evaluate them
+    #                     if max_threshold is not None:
+    #                         if values[0] > max_threshold:  # if value is greater than limit then post event
+    #                             post_event = True
+    #                     if min_threshold is not None:
+    #                         if values[0] < min_threshold:  # if value is less than limit post event
+    #                             post_event = True
 
-                    # Finish status message to send on pub port
-                    modbus_evt.values = values
-                    modbus_evt.names = names
-                    modbus_evt.units = units
-                    et = datetime.datetime.now() - t1
-                    modbus_evt.et = et.total_seconds()
+    #                 # Finish status message to send on pub port
+    #                 modbus_evt.values = values
+    #                 modbus_evt.names = names
+    #                 modbus_evt.units = units
+    #                 et = datetime.datetime.now() - t1
+    #                 modbus_evt.et = et.total_seconds()
 
-                    # Publish status message on pub port if outside of limits or no limits
-                    if post_event:
-                        self.postEvent( modbus_evt )
-                    #check if timer overrun is occuring    
-                    if et.total_seconds() > self.interval / 1000:
-                        time_over_run = True
-                else:
-                    # the polling loop did not complete normally, show a message why
-                    if self.poll_exit:
-                        self.error(modbus_evt,
-                                   "Polling loop deactivated",
-                                   self.device_name,
-                                   ModbusSystem.Errors.AppPollExit)
-                    elif comm_error == True:
-                        self.error(modbus_evt,
-                                   "Polling loop communication error",
-                                   self.device_name,
-                                   ModbusSystem.Errors.CommError)
-                    else:
-                        self.error(modbus_evt,
-                                   "Polling loop timer overrun",
-                                   [self.interval, "msec"],
-                                   ModbusSystem.Errors.PollTimerOverrun)
+    #                 # Publish status message on pub port if outside of limits or no limits
+    #                 if post_event:
+    #                     self.postEvent( modbus_evt )
+    #                 #check if timer overrun is occuring    
+    #                 if et.total_seconds() > self.interval / 1000:
+    #                     time_over_run = True
+    #             else:
+    #                 # the polling loop did not complete normally, show a message why
+    #                 if self.poll_exit:
+    #                     self.error(modbus_evt,
+    #                                "Polling loop deactivated",
+    #                                self.device_name,
+    #                                ModbusSystem.Errors.AppPollExit)
+    #                 elif comm_error == True:
+    #                     self.error(modbus_evt,
+    #                                "Polling loop communication error",
+    #                                self.device_name,
+    #                                ModbusSystem.Errors.CommError)
+    #                 else:
+    #                     self.error(modbus_evt,
+    #                                "Polling loop timer overrun",
+    #                                [self.interval, "msec"],
+    #                                ModbusSystem.Errors.PollTimerOverrun)
 
-                    self.postEvent( modbus_evt )
-                    break
-        else: # nothing to poll
-            pass
+    #                 self.postEvent( modbus_evt )
+    #                 break
+    #     else: # nothing to poll
+    #         pass
             
     # riaps:keep_modbus_poller:end
 
