99b1700247ddd6393ab1c0b6be5d9d42f35e6e26
Scott Eisele <eiselesr@gmail.com>
2023-02-09 23:09:38 +0000

    modbus polling seems to be functional

--- src/riaps/interfaces/modbus/ModbusMasterThread.py
+++ src/riaps/interfaces/modbus/ModbusMasterThread.py
@@ -1,18 +1,36 @@
 import logging
 import threading
+import zmq
 
 from riaps.interfaces.modbus.ModbusInterface import ModbusInterface
+
 
 class ModbusMaster(threading.Thread):
     def __init__(self, path_to_config_file, logger=None):
         super().__init__()
         self.logger = logger if logger else logging.getLogger(__name__)
-        self.stop_polling = threading.Event
+        # TODO: Maybe use getLogger first and check for handlers, then check for
+        #  passed loggers, then use getLogger.
+        self.stop_polling = threading.Event()
+        self.poller = zmq.Poller()
 
-        self.modbus_interface = ModbusInterface(path_to_file=path_to_config_file)
+        self.modbus_interface = ModbusInterface(path_to_file=path_to_config_file, logger=self.logger)
+        self.device_config = self.modbus_interface.device_config
 
     def run(self) -> None:
-        while not self.stop_polling.wait(timeout=1)
+        poll_interval = self.device_config.get("Interval")
+        parameters_to_poll = self.device_config.get("poll")
+
+        if not parameters_to_poll:
+            self.logger.warning("No parameters configured to poll")
+            return
+
+        self.logger.debug(f"parameters_to_poll: {parameters_to_poll}")
+        while not self.stop_polling.wait(timeout=10):
+            for parameter in parameters_to_poll:
+                self.logger.debug(f"poll parameter: {parameter}")
+                self.modbus_interface.read_modbus(parameter=parameter)
+
 
 
 
