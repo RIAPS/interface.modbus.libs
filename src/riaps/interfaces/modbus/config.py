6f562c81efa8501e8c4c0f056a4dd76570d4de96
Scott Eisele <eiselesr@gmail.com>
2023-02-10 17:33:09 +0000

    return paths

--- src/riaps/interfaces/modbus/config.py
+++ src/riaps/interfaces/modbus/config.py
@@ -3,20 +3,21 @@ import logging
 import yaml
 
 
-def load_config_files(path_to_device_list):
+def load_config_paths(path_to_device_list):
 
     with open(path_to_device_list, 'r') as file:
         device_list = yaml.safe_load(file)
 
     global_debug_mode = device_list.get("GlobalDebugMode")
     path_to_configs = device_list["path_to_config_files"]
-    device_configs = {}
+    device_config_paths = {}
     for device_name in device_list["names"]:
         path_to_device_config = f"{path_to_configs}/{device_name}.yaml"
-        with open(path_to_device_config, "r") as file:
-            device_configs[device_name] = yaml.safe_load(file)
+        device_config_paths[device_name] = path_to_device_config
+        # with open(path_to_device_config, "r") as file:
+        #     device_configs[device_name] = yaml.safe_load(file)
 
-    return device_configs, global_debug_mode
+    return device_config_paths, global_debug_mode
 
 
 
