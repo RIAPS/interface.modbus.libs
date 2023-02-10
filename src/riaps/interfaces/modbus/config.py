096cb00eb45187d09f2c8dca0efee522ca36b677
Scott Eisele <eiselesr@gmail.com>
2023-02-10 16:56:12 +0000

    redo config file

--- src/riaps/interfaces/modbus/config.py
+++ src/riaps/interfaces/modbus/config.py
@@ -3,45 +3,20 @@ import logging
 import yaml
 
 
-class Config:
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
-        self.global_debug_mode = 0
-        self.device_configs = {}
+def load_config_files(path_to_device_list):
 
-    def load_config_file(self, path_to_device_component_config):
+    with open(path_to_device_list, 'r') as file:
+        device_list = yaml.safe_load(file)
 
-        device_component_config_file_missing = not os.path.exists(path_to_device_component_config)
-        if device_component_config_file_missing:
-            success = False
-            error_msg = f"Component configuration file does not exist: [{path_to_device_component_config}]"
-            return success, error_msg
+    global_debug_mode = device_list.get("GlobalDebugMode")
+    path_to_configs = device_list["path_to_config_files"]
+    device_configs = {}
+    for device_name in device_list["names"]:
+        path_to_device_config = f"{path_to_configs}/{device_name}.yaml"
+        with open(path_to_device_config, "r") as file:
+            device_configs[device_name] = yaml.safe_load(file)
 
-        with open(path_to_device_component_config, 'r') as file:
-            device_component_config = yaml.safe_load(file)
+    return device_configs, global_debug_mode
 
-        if "GlobalDebugMode" in device_component_config:
-            self.global_debug_mode = device_component_config["GlobalDebugMode"]
-            self.logger.info(
-                f"Debug setting for all devices are overridden: GlobalDebugMode={self.global_debug_mode}")
 
-        for device_name in device_component_config["deviceNames"]:
-            path_to_device_config = f"{device_component_config['path']}/{device_name}.yaml"
-            device_config_file_missing = not os.path.exists(path_to_device_config)
-            if device_config_file_missing:
-                success = False
-                error_msg = f"Device configuration file does not exist: [{path_to_device_config}]"
-                return success, error_msg
 
-            with open(path_to_device_config, 'r') as file:
-                device_config = yaml.safe_load(file)
-
-            self.device_configs[device_name] = device_config[device_name]
-
-        number_of_devices_configured = len(self.device_configs)
-        self.logger.info(f"{number_of_devices_configured} Modbus devices found in configuration:")
-        for k in self.device_configs:
-            self.logger.info(f"{k}")
-        success = True
-        msg = f"Modbus devices found in configuration: {len(self.device_configs)}"
-        return success, msg
